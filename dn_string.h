1f2b2e8af584354f30aaa96e2f59cecb5b9fde2b
SirJoeth3rd <njdewaal@gmail.com>
2025-11-25 18:57:59 +0200

    debug mode for String_cstring

--- dn_string.h
+++ dn_string.h
@@ -4,6 +4,7 @@
 #include "arena.h"
 #include "string.h"
 #include <stdbool.h>
+#include <assert.h>
 
 typedef struct {
   const char* chars;
@@ -15,9 +16,21 @@ typedef struct {
    but perhaps the user does not want to use arena's.
 */
 
+char* String_cstring(Arena*, String);
+String String_from_cstring(char*);
+bool String_cmp(String, const char*);
+
 char* String_cstring(Arena* arena, String str) {
   char* tmp_buffer;
-  tmp_buffer = (char*) arena_alloc(arena, sizeof(char)*(str.length+1));
+  if (arena) {
+    tmp_buffer = (char*) arena_alloc(arena, sizeof(char)*(str.length+1));
+  } else {
+#if defined(NDEBUG)
+    tmp_buffer = (char*) malloc(sizeof(char)*(str.length+1));
+#else
+    assert(("String_cstring requires arena to put chars in", arena));
+#endif
+  }
   memcpy(tmp_buffer, str.chars, str.length);
   tmp_buffer[str.length] = '\0';
   return tmp_buffer;
@@ -41,6 +54,10 @@ bool String_cmp(String str1, const char* cstr) {
     }
   }
   return true;
-} 
+}
+
+void String_cpy(String str, char* buffer) {
+  memcpy(buffer, str.chars, str.length);
+}
 
 #endif
